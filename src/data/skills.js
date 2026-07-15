c61c273b1f68ee37cfb2da19b44235488b4dec8e
Akash Tirkey <akash.tirkeywork@gmail.com>
2026-07-15 14:45:38 +0530

    skills section begins

---
+++ src/data/skills.js
@@ -0,0 +1,27 @@
+const skills = [
+  {
+    category: "Frontend",
+    items: [
+      "HTML",
+      "CSS",
+      "JavaScript(ES6+)",
+      "React",
+      "Bootstrap",
+      "Material UI",
+    ],
+  },
+  {
+    category: "Backend",
+    items: ["Node.js", "Express.js", "MongoDB", "Http", "REST APIs"],
+  },
+  {
+    category: "DataBase",
+    items: ["MongoDB", "Mongoose", "SQL"],
+  },
+  {
+    category: "Tools & Others",
+    items: ["Git", "GitHub", "Linux", "Postman", "Vercel"],
+  },
+];
+
+export default skills;
